3e59a30b84ff796a4ecac7452046f9aadde191c2
Rntk <kvrenat@gmail.com>
2015-10-25 23:07:18 +0600

    base structure ready

--- favicon.erl
+++ favicon.erl
@@ -1,28 +1,51 @@
 -module(favicon).
--export([start/1, favicon_searcher/1]).
+-export([start/2, favicon_searcher/2]).
 
-start(Path, Threads) when if_string(Path), is_file(Path), is_integer(Threads), Threads > 0 -> 
+start(File_name, Threads) -> 
     start_thread(Threads),
+    {ok, Binary_urls} = file:read_file(File_name),
+    Raw_urls = binary:split(Binary_urls, <<"\n">>, [global]),
+    send_urls(Raw_urls).
     
     
-start_thread(0) -> io.format("Spawning done~n", [])
+start_thread(0) -> io:format("Spawning done~n", []);
 start_thread(Number) -> 
     spawn(favicon, favicon_searcher, [self(), true]),
-    start_thread(Number - 1)
-                   
-stop_thread([]) -> io.format("Stopping done~n", []);
-stop_thread([Head|Tail]) -> 
-    Head ! finish,
-    stop_thread(Tail).
+    start_thread(Number - 1).
 
-                         
-favicon_searcher(Server_PID, Need_notify) -> 
-    if 
-        Need_notify -> Server_PID ! self()
-    end,
+favicon_searcher(Server_PID, true) -> 
+    Server_PID ! {self()},
+    favicon_searcher(Server_PID, false);
+favicon_searcher(Server_PID, false) -> 
+    Timeout = 5000,
     receive
-        finish -> io.format("Worker ~w is off~n", [self()]);
+        finish -> io:format("Worker ~w is off~n", [self()]);
         {url, Url} -> 
-            io.format("Process: ~n", [Url]),
+            io:format("Process ~w: ~n", [Url]),
+            Server_PID ! {self(), Url},
             favicon_searcher(Server_PID, false)
+        after Timeout ->
+            io:format("Worker ~w is off after waiting ~w ms~n", [self(), Timeout])
     end.
+
+send_urls([]) -> 
+    Timeout = 5000,
+    receive
+        {PID, Favicon_url} -> 
+            PID ! finish,
+            save_favicon_url(Favicon_url),
+            send_urls([])
+    after Timeout ->
+        io:format("Finish after waiting ~w ms~n", [Timeout])
+    end;
+send_urls([Raw_url|Raw_urls]) -> 
+    Url = binary_to_atom(Raw_url, utf8),
+    receive
+        {PID} -> PID ! {url, Url};
+        {PID, Favicon_url} -> 
+            PID ! {url, Url},
+            save_favicon_url(Favicon_url)
+    end,
+    send_urls(Raw_urls).
+
+save_favicon_url(Url) -> io:format("Save url ~s~n", [Url]).
