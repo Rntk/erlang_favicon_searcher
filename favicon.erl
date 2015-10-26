656b057e4b8142f6348ba419b8dfd28138c9a65f
Rntk <kvrenat@gmail.com>
2015-10-26 21:37:46 +0600

    add saver worker

--- favicon.erl
+++ favicon.erl
@@ -1,56 +1,67 @@
 -module(favicon).
--export([start/2, favicon_searcher/2]).
+-export([start/3, favicon_searcher/1, saver_worker/1]).
 
-start(File_name, Threads_number) -> 
+start(File_name, Threads_number, Result_file) -> 
+    %inet:start(),
+    register(saver, spawn(favicon, saver_worker, [Result_file])),
     start_thread(Threads_number),
     {ok, Binary_urls} = file:read_file(File_name),
     Raw_urls = binary:split(Binary_urls, <<"\n">>, [global]),
     send_urls(Raw_urls, Threads_number).
-
     
 start_thread(0) -> io:format("Spawning done~n", []);
 start_thread(Number) -> 
-    spawn(favicon, favicon_searcher, [self(), true]),
+    spawn(favicon, favicon_searcher, [self()]),
     start_thread(Number - 1).
 
-favicon_searcher(Server_PID, true) -> 
+extract_favicon_url(Page) -> Page.
+
+favicon_searcher(Server_PID) -> 
     Server_PID ! {pid, self()},
-    favicon_searcher(Server_PID, false);
-favicon_searcher(Server_PID, false) -> 
     Timeout = 5000,
     receive
         finish -> io:format("Worker ~w is off~n", [self()]);
         {url, Url} -> 
-            io:format("Process ~w: ~n", [Url]),
-            Server_PID ! {self(), Url},
-            favicon_searcher(Server_PID, false)
+            io:format("Process ~s ~n", [Url]),
+            %Page = httpc:request(Url),
+            Page = Url,
+            Favicon_url = extract_favicon_url(Page),
+            saver ! {url, Favicon_url},
+            favicon_searcher(Server_PID)
         after Timeout ->
             io:format("Worker ~w is off after waiting ~w ms~n", [self(), Timeout])
     end.
 
 send_urls([], 0) -> 
+    saver ! finish,
     io:format("All done~n", []);
 send_urls([], Threads_number) -> 
     Timeout = 5000,
     receive
         {pid, PID} -> 
             PID ! finish,
-            send_urls([], Threads_number - 1);
-        {PID, Favicon_url} -> 
-            PID ! finish,
-            save_favicon_url(Favicon_url),
             send_urls([], Threads_number - 1)
     after Timeout ->
         io:format("Finish after waiting ~w ms~n", [Timeout])
     end;
 send_urls([Raw_url|Raw_urls], Threads_number) -> 
-    Url = binary_to_atom(Raw_url, utf8),
+    Url = binary:bin_to_list(Raw_url),
     receive
-        {pid, PID} -> PID ! {url, Url};
-        {PID, Favicon_url} -> 
-            PID ! {url, Url},
-            save_favicon_url(Favicon_url)
+        {pid, PID} -> PID ! {url, Url}
     end,
     send_urls(Raw_urls, Threads_number).
 
-save_favicon_url(Url) -> io:format("Save url ~s~n", [Url]).
+saver_worker(Result_file) ->
+    receive
+        finish -> io:format("Saver is off~n", []);
+        {url, Url} -> 
+            save_favicon_url(Url, Result_file),
+            saver_worker(Result_file)
+    end.
+
+save_favicon_url(Url, Result_file) -> 
+    Writed = file:write_file(Result_file, list_to_binary(Url ++ "\n"), [append]),
+    case Writed of
+        ok -> io:format("Saved url: ~s~n", [Url]);
+        {error, Reason} -> io:format("Not saved url: ~s. Reason ~w~n", [Url, Reason])
+    end.
