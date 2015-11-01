6093409f0885261e56c517f1f6a6ed5e6b9f853b
Rntk <kvrenat@gmail.com>
2015-11-01 23:17:34 +0600

    update readme, upddate gitignore, update tests

--- tests/favicon_SUITE.erl
+++ tests/favicon_SUITE.erl
@@ -2,13 +2,29 @@
 -compile(export_all).
 
 all() -> [
+    module_exists,
     slash,
     process_favicon_url
 ].
 
-slash(_) -> 
-    "/" = favicon:slash("http://example.com"),
-    [] = favicon:slash("http://example.com/").
+module_exists(_) -> 
+    true = code:add_path("."),
+    true = code:add_path(".."),
+    true = code:add_path("../.."),
+    true = code:add_path("../../.."),
+    {module, favicon} = code:load_file(favicon).
 
-process_favicon_url(_) -> "".
-    %favicon:process_favicon_url("http://example.com", "http://example.com").
+slash(_) -> 
+    Url = "http://example.com",
+    "/" = favicon:slash(Url),
+    [] = favicon:slash(Url ++ "/").
+
+process_favicon_url(_) -> 
+    "http://example.com/favicon.ico" = favicon:process_favicon_url("http://example.com/favicon.ico", "http://example.com"),
+    "https://example.com/favicon.ico" = favicon:process_favicon_url("https://example.com/favicon.ico", "https://example.com"),
+    "http://example.com/favicon.ico" = favicon:process_favicon_url("//example.com/favicon.ico", "http://example.com"),
+    "https://example.com/favicon.ico" = favicon:process_favicon_url("//example.com/favicon.ico", "https://example.com"),
+    "http://example.com/favicon.ico" = favicon:process_favicon_url("/favicon.ico", "http://example.com"),
+    "https://example.com/favicon.ico" = favicon:process_favicon_url("/favicon.ico", "https://example.com"),
+    "http://example.com/favicon.ico" = favicon:process_favicon_url("favicon.ico", "http://example.com"),
+    "https://example.com/favicon.ico" = favicon:process_favicon_url("favicon.ico", "https://example.com").
